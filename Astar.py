74628c30606606975b186775fe5065f99a77b568
Raghav <raghav706@gmail.com>
2016-10-02 23:26:14 -0400

    Added A star

---
+++ Astar.py
@@ -0,0 +1,129 @@
+# Author: Christian Careaga (christian.careaga7@gmail.com)
+# A* Pathfinding in Python (2.7)
+# Please give credit if used
+
+import numpy
+import math
+from heapq import *
+
+
+def heuristic(a, b):
+    
+    xd = b[0] - a[0]    
+    yd = b[1] - a[1]   
+    xd = abs(xd) 
+    yd = abs(yd)
+    d = sqrt(2)*min(xd,yd) + max(xd,yd) + min(xd,yd)
+    return(d)
+
+def movement(array,a, b):
+
+    river = [3,5,7,9]
+    #Determining direction, 1 for diagonal, 0 for horizontal-vertical
+    if(abs(b[1]-a[1]) + abs(b[0]-a[1]))==2:
+        direction = 1
+    else:
+        direction = 0
+
+    #cost from unblocked to unblocked
+    if array[a[0]][a[1]]==1 and array[b[0]][b[1]]==1:
+        if direction==1:
+            #cost = unblocked to unblocked - diagonal
+        else:
+            cost =
+
+    #cost from hard to unblocked or vice versa
+    if (array[a[0]][a[1]]==1 and array[b[0]][b[1]]==2) or (array[a[0]][a[1]]==2 and array[b[0]][b[1]]==1):
+        if direction==1:
+            #cost = 
+        else:
+            cost = 
+
+    #cost from hard to hard
+    if (array[a[0]][a[1]]==2 and array[b[0]][b[1]]==2):
+        if direction==1:
+            cost = 
+        else:
+            cost =
+
+    #cost from hard to river or vice versa
+    if ((array[a[0]][a[1]] in river) and array[b[0]][b[1]]==2) or ((array[b[0]][b[1]] in river) and array[a[0]][a[1]]==2):
+        if direction==1:
+            cost = 
+        else:
+            cost =
+
+    #cost from unblocked to river or vice versa
+    if ((array[a[0]][a[1]] in river) and array[b[0]][b[1]]==1) or ((array[b[0]][b[1]] in river) and array[a[0]][a[1]]==1):
+        if direction==1:
+            cost = 
+        else:
+            cost =
+
+def astar(array, start, goal):
+
+    neighbors = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
+
+    close_set = set()
+    came_from = {}
+    gscore = {start:0}
+    fscore = {start:heuristic(start, goal)}
+    oheap = []
+
+    heappush(oheap, (fscore[start], start))
+    
+    while oheap:
+
+        current = heappop(oheap)[1]
+
+        if current == goal:
+            data = []
+            while current in came_from:
+                data.append(current)
+                current = came_from[current]
+            return data
+
+        close_set.add(current)
+        for i, j in neighbors:
+            neighbor = current[0] + i, current[1] + j            
+            if 0 <= neighbor[0] < array.shape[0]:
+                if 0 <= neighbor[1] < array.shape[1]:                
+                    if array[neighbor[0]][neighbor[1]] == 0:
+                        continue
+                else:
+                    # array bound y walls
+                    continue
+            else:
+                # array bound x walls
+                continue
+            
+            tentative_g_score = gscore[current] + movement(array, current, neighbor)                
+            if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
+                continue
+    
+            if  tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]:
+                came_from[neighbor] = current
+                gscore[neighbor] = tentative_g_score
+                fscore[neighbor] = tentative_g_score + heuristic(neighbor, goal)
+                heappush(oheap, (fscore[neighbor], neighbor))
+                
+    return False
+
+'''Here is an example of using my algo with a numpy array,
+   astar(array, start, destination)
+   astar function returns a list of points (shortest path)'''
+
+nmap = numpy.array([
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,0,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,1,1,1,1,1,1,1,1,1,1,1,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,0,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,0,1,1,1,1,1,1,1,1,1,1,1,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,0,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
+    
+print astar(nmap, (0,0), (10,13))
